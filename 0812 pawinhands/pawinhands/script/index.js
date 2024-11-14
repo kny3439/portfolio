b58cae653b98d5e63187b7fc7d35a84455bf94e9
김나연 <kny8664@naver.com>
2024-11-14 12:11:35 +0900

    appProcess

--- 0812 pawinhands/pawinhands/script/index.js
+++ 0812 pawinhands/pawinhands/script/index.js
@@ -9,3 +9,42 @@ setInterval(function () {
     $('.slide').fadeOut()
     $('.slide').eq(idx).fadeIn()
 }, 4000);
+
+
+const listElems = document.querySelectorAll('nav ul li')
+const section = document.querySelectorAll('section')
+
+listElems.forEach((li) => {
+    const link = li.querySelector('a[href^="#sect"]')
+    link.addEventListener('click', function (e) {
+        e.preventDefault()
+        const href = this.getAttribute('href');
+        const sectTop = document.querySelector(href);
+        window.scrollTo({
+            top: sectTop.offsetTop,
+            behavior: 'smooth'
+        })
+    })
+})
+
+// section.forEach((sect, i) => {
+
+//     sect.addEventListener('wheel', function (e) {
+//         e.preventDefault()
+//         let delta = e.deltaY
+//         if (delta < 0 && i === 0) {
+
+//             return
+//         } else if (delta > 0 && i === section.length - 1) {
+
+//             return
+//         }
+
+//         let sectTop = delta < 0 ? sect.previousElementSibling : sect.nextElementSibling
+//         if (sectTop) {
+//             let targetSectTop = sectTop.offsetTop
+//             window.scrollTo({ top: targetSectTop, behavior: 'smooth' })
+//         }
+
+//     })
+// })
